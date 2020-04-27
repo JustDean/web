710012f1738f1fc889a194e6e714a1074df24081
JustDean <thedetectivl@gmail.com>
2020-04-27 07:40:43 +0300

    gunicorn configuration and app added

--- init_test.sh
+++ init_test.sh
@@ -1,2 +1,2 @@
-sudo ln -s /home/dean/git/web/etc/nginx_test.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/dean/git/web/etc/nginx_test.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
