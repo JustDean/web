679a6dd19fbf3328c5f7ad29c834e715ab19ada6
JustDean <thedetectivl@gmail.com>
2020-05-05 18:12:23 +0300

    mysql commands added

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@ sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepic_web;"
+mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
