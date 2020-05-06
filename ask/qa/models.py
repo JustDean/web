2c7c08b3e80d9378aab4dd7d6d32abb3de447509
JustDean <thedetectivl@gmail.com>
2020-05-06 12:59:04 +0300

    models.py adjusted. model types changed

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,30 +1,28 @@
 from django.db import models
 
-from django.contrib.auth.models import User as DjangoUser
-
-
-class User(DjangoUser):
-    pass
+from django.contrib.auth.models import User
 
 
 class QuestionManager(models.Manager):
+    use_in_migrations = True
+
     def new(self):
         return self.order_by('-date')
-    
+
     def popular(self):
         return self.oreder_by('-rating')
 
 
 class Question(models.Model):
     objects = QuestionManager()
+
     title = models.CharField(max_length=200)
     text = models.TextField()
-    added_at = models.DateTimeField(auto_now_add=True, name='date')
-    rating = models.PositiveIntegerField(default=0)
-    author = models.OneToOneField(
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='question_author'
     )
     likes = models.ManyToManyField(
         User,
@@ -35,11 +33,11 @@ class Question(models.Model):
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    question = models.OneToOneField(
+    question = models.ForeignKey(
         Question,
         on_delete=models.CASCADE,
     )
-    author = models.OneToOneField(
+    author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
     )
--- init.sh
+++ init.sh
@@ -5,5 +5,6 @@ sudo /etc/init.d/mysql start
 mysql -uroot -e "create database stepic_web;"
 mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
 sudo pip3 install --upgrade django==2.0.7
-python3 /web/ask/manage.py makemigrations
-python3 /web/ask/manage.py migrate
+rm -rf ~/web/ask/qa/migrate
+python3 ~/web/ask/manage.py makemigrates
+python3 ~/web/ask/manage.py migrate
