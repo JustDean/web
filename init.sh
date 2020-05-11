ad48fa37b3f4c07b7ac962340ada0f45dba169a6
JustDean <thedetectivl@gmail.com>
2020-05-11 10:45:11 +0300

    bootstrap initialized

--- init.sh
+++ init.sh
@@ -5,6 +5,10 @@ sudo /etc/init.d/mysql start
 mysql -uroot -e "create database stepic_web;"
 mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
 sudo pip3 install --upgrade django==2.0.7
-rm -rf ~/web/ask/qa/migrate
-python3 ~/web/ask/manage.py makemigrates
-python3 ~/web/ask/manage.py migrate
+
+# tip: change settings.py - set different default database
+
+# then apply as following:
+# rm -rf ~/web/ask/qa/migrate
+# python3 ~/web/ask/manage.py makemigrates
+# python3 ~/web/ask/manage.py migrate
