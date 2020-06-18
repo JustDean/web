ae639f41128457e14c9a42e6518c75d6f561d55a
Dean <thedetectivl@gmail.com>
2020-06-18 13:40:18 +0300

    ready for task 3

--- init.sh
+++ init.sh
@@ -3,7 +3,11 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #task 2
-gunicorn -c /home/box/web/etc/gunicorn.conf.py hello:app
+# gunicorn -c /home/box/web/etc/gunicorn.conf.py hello:app
+#task 3
+sudo pip3 install --upgrade django==2.0.7
+cd /home/box/web/ask && gunicorn -c /home/box/web/etc/gunicorn.conf.py ask.wsgi
+
 # sudo /etc/init.d/nginx restart
 # sudo /etc/init.d/mysql start
 # mysql -uroot -e "create database stepic_web;"
--- local_init.sh
+++ local_init.sh
@@ -3,4 +3,5 @@ sudo rm -rf /etc/nginx/sites-enabled/*
 sudo ln -sf /home/dean/web/etc/local_nginx.conf /etc/nginx/sites-enabled/nginx.conf
 sudo /etc/init.d/nginx restart
 #task 2
-gunicorn -c /home/dean/web/etc/gunicorn.conf.py hello:app
+# gunicorn -c /home/dean/web/etc/gunicorn.conf.py /ask
+#task 3
