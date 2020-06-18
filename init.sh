7e5553aa10179ed7d650f08ea42b358294b00f16
Dean <thedetectivl@gmail.com>
2020-06-18 17:11:54 +0300

    init changed

--- init.sh
+++ init.sh
@@ -1,23 +1,15 @@
-#part 0
+# updating django
 sudo pip3 install --upgrade django==2.0.7
-#task 1
+# setting nginx 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-#task 2
-# gunicorn -c /home/box/web/etc/gunicorn.conf.py hello:app
-#task 3
+# setting mysql database
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database web;"
+mysql -uroot -e "grant all privileges on web.* to 'box'@'localhost' with grant option;"
+rm -rf ~/web/ask/qa/migrate
+python3 ~/web/ask/manage.py makemigrates
+python3 ~/web/ask/manage.py migrate
+# starting gunicorn
 cd /home/box/web/ask && gunicorn -c /home/box/web/etc/gunicorn.conf.py ask.wsgi
-
-# sudo /etc/init.d/nginx restart
-# sudo /etc/init.d/mysql start
-# mysql -uroot -e "create database stepic_web;"
-# mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
-# sudo pip3 install --upgrade django==2.0.7
-
-# tip: change settings.py - set different default database
-
-# then apply as following:
-# rm -rf ~/web/ask/qa/migrate
-# python3 ~/web/ask/manage.py makemigrates
-# python3 ~/web/ask/manage.py migrate
--- local_init.sh
+++ local_init.sh
@@ -7,6 +7,6 @@ sudo /etc/init.d/nginx restart
 #task 3
 cd ask && gunicorn -c /home/dean/web/etc/gunicorn.conf.py ask.wsgi
 #task 4
-rm -rf ~/web/ask/qa/migrate
-python3 ~/web/ask/manage.py makemigrates
-python3 ~/web/ask/manage.py migrate
+# rm -rf ~/web/ask/qa/migrate
+# python3 ~/web/ask/manage.py makemigrates
+# python3 ~/web/ask/manage.py migrate
