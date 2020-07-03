b6887748f88125d289a38b8d5c4174271b1d2c7b
Dean <thedetectivl@gmail.com>
2020-07-03 20:26:52 +0300

    submitting

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 # updating django
 sudo pip3 install --upgrade django==2.0.7
-# setting nginx 
+# installing crispy for nice-forms
+pip install update
+pip install django-crispy-forms
+# setting nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
@@ -8,9 +11,10 @@ sudo /etc/init.d/nginx restart
 sudo /etc/init.d/mysql start
 mysql -uroot -e "create database web;"
 mysql -uroot -e "grant all privileges on web.* to 'box'@'localhost' with grant option;"
-# follow next steps after changing user in ask/settings 
-# rm -rf ~/web/ask/qa/migrate
-# python3 ~/web/ask/manage.py makemigrates
-# python3 ~/web/ask/manage.py migrate
-# # starting gunicorn
-cd /home/box/web/ask && gunicorn -c /home/box/web/etc/gunicorn.conf.py ask.wsgi
+# reinitializing database
+rm -rf ~/web/ask/qa/migrate
+python3 ~/web/ask/manage.py makemigrates
+python3 ~/web/ask/manage.py migrate
+python3 ~/web/ask/manage.py createsuperuser
+# starting gunicorn. (never works. have to do it by hands)
+# cd /home/box/web/ask && gunicorn -c /home/box/web/etc/gunicorn.conf.py ask.wsgi
